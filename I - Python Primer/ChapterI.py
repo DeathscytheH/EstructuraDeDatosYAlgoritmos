418faea5b5c137fb4ed8cbaea47d02f1aedfa26c
DeathscytheH <alejandro.deathscythe@gmail.com>
2014-07-08 21:11:05 -0500

    Chapter I
    Mas ejercicios del capitulo I

--- I - Python Primer/ChapterI.py
+++ I - Python Primer/ChapterI.py
@@ -45,3 +45,93 @@ def is_even(k):
     even, False otherwise. Do not use multiplication, module or division
     """
     return True if (-1)**abs(k)==1 else False
+
+#R-1.3
+
+def minmax(data):
+    """
+    minmax(data) : Toma una secuencia de uno o mas numeros y regresa el mas 
+    pequeno y el mas grande, en una tupla de tamano dos. No usar las funciones 
+    min y max de python
+    """
+    arreglo=sorted(data)
+    return (arreglo[0], arreglo[len(arreglo)-1])
+    
+#R-1.4
+
+def suma_cuadrados(n):
+    """
+    Toma un entero positivo y regresa la suma de los cuadrados de todos los 
+    enteros positivos menores a n.
+    """
+    if n<=0:
+        return 0
+    return (n-1)**2+suma_cuadrados(n-1)
+
+#R-1.5
+
+def suma_cuadrados_lista(n):
+    """
+    Realizar una suma de cuadrados utilizando comprension de listas
+    """
+    return sum([e**2 for e in range(n)])
+
+#R-1.6
+
+def suma_cuadrados_par(n):
+    """
+    Realizar una suma de cuadrados utilizando comprension de listas de los 
+    numeros pares.
+    """
+    if n<=0:
+        return 0
+    if n%2==0:
+        return n**2 + suma_cuadrados_par(n-1)
+    else:
+        return 0 + suma_cuadrados_par(n-1)
+
+#R-1.7
+
+def suma_cuadrados_par_lista(n):
+    """
+    Realizar una suma de cuadrados utilizando comprension de listas de los 
+    numeros pares.
+    """
+    return sum([e**2 for e in range(n) if e%2==0])
+
+#R-1.8
+
+"""
+Una string s tiene tamano n, y la expresion s[k] se usa para el indice -n<=k<0.
+Cual es equivalente al indice j>=0 de tal manera que s[j] haga referencia al 
+mismo elemento.
+
+R:
+    [0,1,2,3]
+    [-4,-3,-2,-1]
+s[k]==s[j+n]
+"""
+
+#R-1.9
+"""
+Parametros para range y obtener los valores 50, 60, 70, 80
+"""
+
+print range(50,81,10)
+
+#R-1.10
+
+"""
+Parametros para range y obtener los valores 8,6,4,2,0,-2,-4,-6,-8
+"""
+
+print range(-8,9,2)[::-1]
+
+#R-1.11
+
+"""
+Demuestra la comprension de listas para obtener la lista:
+    [1,2,4,8,16,32,64,128,256]
+"""
+
+print [2**e for e in range(9)]
